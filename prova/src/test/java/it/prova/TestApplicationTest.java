cf2e1928857e06464c82515361e423cd44661b9a
DESKTOP-CEVEJPL\Andrea <andreaserrecchia@hotmail.it>
2018-05-16 16:33:44 +0200

    Applicazione di prova Spring Boot + Gradle

---
+++ prova/src/test/java/it/prova/TestApplicationTest.java
@@ -0,0 +1,17 @@
+package it.prova;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
