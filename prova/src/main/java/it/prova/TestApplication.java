8362a36e5991e8f4d34d5e47b679cae0d0ed9399
DESKTOP-RB7UUS0\Davide Soldani <davide71295@gmail.com>
2018-05-22 11:36:07 +0200

    Aumento complessita dell applicazione per favorire lo sviluppo di test

--- prova/src/main/java/it/prova/TestApplication.java
+++ prova/src/main/java/it/prova/TestApplication.java
@@ -8,6 +8,23 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
-		System.out.println("Hello Word!");
+		
+		//inizializzo un oggetto Hello contenente la stringa Hello World!
+		HelloObject hello = new HelloObject();
+		
+		//stampo la stringa dell'oggetto hello
+		System.out.println(hello.getHello()); 
+		
+		//creo un'array lungo quanto la stringa hello
+		char helloArray[] = new char[hello.getHello().length()]; 
+		
+		//Popolo l'array helloArray con gli elementi della stringa hello
+		for(int i=0; i<hello.getHello().length(); i++) {
+			helloArray[i]=hello.getHello().charAt(i);
+		}
+		
+		//Stampo elemento per elemento helloArray
+		for(char element:helloArray)
+			System.out.println(element);
 	}
 }
--- prova/src/test/java/it/prova/TestApplicationTest.java
+++ prova/src/test/java/it/prova/TestApplicationTest.java
@@ -2,6 +2,7 @@ package it.prova;
 
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,5 +14,7 @@ public class TestApplicationTest {
 	@Test
 	public void contextLoads() {
 	}
+	
+	
 
 }
